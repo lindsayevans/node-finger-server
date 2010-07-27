ba3939a18c6d685976943e66aaa9034c1b9899d9
Lindsay Evans <linz@linz.id.au>
2010-07-27 21:29:46 +1000

    Initial server mumbo-jumo

---
+++ finger-server.js
@@ -0,0 +1,29 @@
+var net = require('net'),
+    sys = require('sys'),
+    listen_address = '127.0.0.1'
+    listen_port = 79
+;
+
+net.createServer(function(socket){
+
+  socket.setEncoding("utf8");
+  console.log('Connection established, remote address: ' + socket.remoteAddress);
+
+  socket.on("data", function(data){
+    console.log('Incoming request: [' + data + ']');
+
+    if(data.match(/lindsay/i)){
+      socket.write('lindsay is online & being awesome');
+      socket.end();
+    }
+
+  });
+  socket.on("end", function(){
+    console.log('Connection closed, remote address: ' + socket.remoteAddress);
+    socket.end();
+  });
+
+}).listen(listen_port, listen_address);
+
+console.log('Finger server listening on ' + listen_address + ':' + listen_port);
+
